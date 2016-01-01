cf6f7001cec740bc79ca19ca7a99b0a788d71c5c
Brunation11 <bzatta@gmail.com>
2016-01-01 15:22:00 -0500

    Add association and validation tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:comment)).to be_valid
+  end
+  it {is_expected.to validate_presence_of(:commentor)}
+  it {is_expected.to validate_presence_of(:content)}
+  it {is_expected.to validate_presence_of(:commentable)}
+  it {is_expected.to validate_length_of(:content).is_at_least(10)}
+  it {is_expected.to belong_to(:commentor)}
+  it {is_expected.to belong_to(:commentable)}
 end
