4aec8572685bd6d67ebe05c2420ed5b83535b3b9
Jack Huang <email": null>
2016-01-01 16:42:00 -0500

    Rename question to questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe QuestionController, type: :controller do
+RSpec.describe QuestionsController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
