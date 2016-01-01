d2cdc087c0e58f7f7939c812ed3fda4c62a4cc85
Jack Huang <email": null>
2016-01-01 14:20:45 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
+  root to: 'questions#index'
   resources :questions, shallow: true do
     resources :answers, except: :show
   end
 
-  resources :comments, only: [:new, :create, :delete, :update]
-
+  resources :comments, except: [:index, :show]
 end
