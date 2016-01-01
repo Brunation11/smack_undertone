f4ed48a01c122319c1704006fc5d55b6b51da454
Jack Huang <email": null>
2016-01-01 16:15:19 -0500

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,47 @@
 class CommentsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    new_comment = Comment.new(comments_param)
+    if new_comment.save
+      redirect_to root_path
+    else
+      flash[:warning] = "There was a problem saving the comment"
+      render :new
+    end
+  end
+
+  def edit
+    @update_comment = Comment.find(params[:id])
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if valid_user(comment) && comment.update_attributes(update_param)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+
+  private
+  def comments_param
+    params.require(:comment).permit(:commentor_id, :content, :commentable_type, :commentable_id)
+  end
+
+  def update_param
+    params.require(:update_comment).permit(:content)
+  end
+
+  def valid_user(comment)
+    current_user.id == comment.commentor_id
+  end
 
 end
