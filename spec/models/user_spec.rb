f4164c6048092ba9dffe91307902ff0a848d239d
Brunation11 <bzatta@gmail.com>
2016-01-01 12:58:26 -0500

    Comment out model specs to prevent interference with user model tests, add validation and association tests to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+  it {is_expected.to validate_presence_of(:username)}
+  it {is_expected.to validate_uniqueness_of(:username)}
+  it {is_expected.to validate_length_of(:username).is_at_least(4)}
+  it {is_expected.to validate_presence_of(:email)}
+  it {is_expected.to have_secure_password}
+  it {is_expected.to validate_presence_of(:password)}
+  it {is_expected.to validate_length_of(:password).is_at_least(6)}
+  it {is_expected.to validate_confirmation_of(:password)}
+  it {is_expected.to have_many(:questions)}
+  it {is_expected.to have_many(:answers)}
+  it {is_expected.to have_many(:comments)}
+  it {is_expected.to have_many(:votes)}
 end
