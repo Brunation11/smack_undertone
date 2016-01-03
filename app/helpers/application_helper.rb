c6c40799f026800c31d60c348b7a420c6a0f3e07
Eunice Do <eunicedx@gmail.com>
2016-01-03 14:49:15 -0500

    Add datetime formatting methods for user profile and question show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def user_profile_date(d)
+    return "" unless d
+    d.strftime("%b %e, %Y")
+  end
+
+  def question_datetime(d)
+    return "" unless d
+    d.strftime("%b %e\'%y at %l:%M")
+  end
 end
