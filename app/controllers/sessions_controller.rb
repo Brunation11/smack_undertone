0f49b222bcc4ab2b07905c8036472dfd594b5114
Eunice Do <eunicedx@gmail.com>
2016-01-03 00:46:45 -0500

    Fix sessions controller logic :D

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,12 @@ class SessionsController < ApplicationController
   skip_before_action :ensure_logged_in
 
   def new
-    @user = User.find_or_initialize_by(username_param)
+    # @user = User.find_or_initialize_by(username_param)
   end
 
   def create
-    user = User.find_by(username_param)
+    user = User.find_by(username: username_param)
     if user && user.authenticate(password_param)
-    # user = User.find_by(username: username_param)
-    # if user && user.authenticate(password_param)
       session[:user_id] = user.id
       redirect_to root_path
     else
@@ -24,13 +22,11 @@ class SessionsController < ApplicationController
 
   private
     def username_param
-      params.permit(:username)
-      # params[:session][:username]
+      params[:session][:username]
     end
 
     def password_param
-      params.permit(:password)[:password]
-      # params[:session][:password]
+      params[:session][:password]
     end
 
 end
