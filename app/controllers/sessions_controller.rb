a1061659edf15a28aa8e247576102d7a034220d7
Eunice Do <eunicedx@gmail.com>
2016-01-02 20:50:01 -0500

    Fix session params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_logged_in
 
   def new
   end
 
   def create
-    user = User.find_by(username_param)
+    user = User.find_by(username: username_param)
     if user && user.authenticate(password_param)
       session[:user_id] = user.id
       redirect_to root_path
@@ -20,11 +21,11 @@ class SessionsController < ApplicationController
 
   private
     def username_param
-      params.permit(:username)
+      params[:session][:username]
     end
 
     def password_param
-      params.permit(:password)[:password]
+      params[:session][:password]
     end
 
 end
