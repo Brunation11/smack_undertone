efaf67cfdf4177c8a8693a8febf50a25808d96c6
Jack Huang <email": null>
2016-01-03 14:15:31 -0500

    ajax signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,22 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    render :new if !request.xhr?
+    if request.xhr?
+      render :new, layout: false
+    else
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
+      if request.xhr?
+        session[:user_id] = @user.id
+        render 'questions/newest', layout: false
+      else
+        session[:user_id] = @user.id
+        redirect_to root_path
+      end
     else
       render 'new'
     end
