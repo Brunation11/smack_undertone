679fd5afc18ebc98f73ab87660bd660e28218036
Eunice Do <eunicedx@gmail.com>
2016-01-03 14:48:27 -0500

    Add Faker to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-us = User.create!(email:'steven@example.com', password:'abcdefghij', username:'steven')
-u2 = User.create!(email:'euni@example.com', password:'123456', username:'euni')
+us = User.create!(email:'steven@example.com', password:'abcdefghij', password_confirmation:'abcdefghij', username:'steven')
+u2 = User.create!(email:'euni@example.com', password: '123456', password_confirmation: '123456', username:'euni')
 
-qu = us.questions.create!(title:'a?', content:'babdsdfadfafdafafdaf')
-q2 = u2.questions.create!(title: 'Filler Question', content: 'asdfasdfasdfasdf')
-q3 = u2.questions.create!(title: 'Polymorpha-what?', content: 'asdfasdfasdfasdf')
+qu = us.questions.create!(title: "How can I #{Faker::Company.bs}?", content: Faker::Hipster.paragraphs(4).join)
+q2 = u2.questions.create!(title: "What does #{Faker::Company.bs} mean?", content: Faker::Hipster.paragraphs(4).join)
+q3 = u2.questions.create!(title: 'Polymorpha-what?', content: Faker::Hipster.paragraphs(4).join)
 
-an = qu.answers.create!(author:User.last, content:'abcdefghigdfaddsafasfads')
-an.comments.create!(content: 'fgcfgdfgcfgfxg', commentor: User.last)
+an = qu.answers.create!(author:User.last, content: Faker::Hacker.say_something_smart)
+an.comments.create!(content: Faker::Hacker.say_something_smart, commentor: User.last)
 
 qu.votes.create!(value: 1, user: us)
 an.votes.create!(value: -1, user: us)
