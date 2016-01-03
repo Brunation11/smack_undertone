681ed332dcbd0d176899e7188664f677c54042c1
Eunice Do <eunicedx@gmail.com>
2016-01-02 19:30:18 -0500

    Fix highest voted question DB query

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,26 @@
 class QuestionsController < ApplicationController
-  skip_before_action :ensure_logged_in, only: :index, :show
+  skip_before_action :ensure_logged_in, only: [:index, :show]
 
   def index
     @qs = Question.all
     @most_recent = Question.order(created_at: :desc).limit(10)
     @trending = Question.order(updated_at: :desc).limit(10)
-    @highest_voted = Vote.
-      where(votable_type: 'Question').
-      order('sum_value DESC').
-      group(:votable_id).
-      limit(10).
-      sum(:value)
+    # @highest_voted = Vote.
+    #   where(votable_type: 'Question').
+    #   order('sum_value DESC').
+    #   group(:votable_id).
+    #   limit(10).
+    #   sum(:value)
+    # @highest_voted = @highest_voted.map { |x| Question.find(x[0]) }
+
+    @highest_voted = Question.joins(
+      # Question.joins = SELECT questions.* FROM questions 
+      'JOIN (SELECT votable_id, SUM(value) AS score  
+      FROM votes WHERE votable_type = \'Question\'
+      GROUP BY votable_id
+      ORDER BY score DESC) v ON questions.id = v.votable_id'
+      # Sub-join table with alias of v -- where v contains votable_id and sum of vote values (temp in memory) -- on questions.id and votable_id.
+    )
   end
 
   def new
