e3314be0de73a48efee458b26ee8c0c6addd4712
Eunice Do <eunicedx@gmail.com>
2016-01-03 22:38:01 -0500

    Fix highest_voted question sorting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,22 +17,22 @@ class QuestionsController < ApplicationController
 
   def highest_voted
     @questions = Question.all
-        # @highest_voted = Vote.
-    #   where(votable_type: 'Question').
-    #   order('sum_value DESC').
-    #   group(:votable_id).
-    #   limit(10).
-    #   sum(:value)
-    # @highest_voted = @highest_voted.map { |x| Question.find(x[0]) }
+      @highest_voted = Vote.
+      where(votable_type: 'Question').
+      order('sum_value DESC').
+      group(:votable_id).
+      limit(10).
+      sum(:value)
+    @highest_voted = @highest_voted.map { |x| Question.find(x[0]) }
 
-    @highest_voted = Question.joins(
-      # Question.joins = SELECT questions.* FROM questions
-      'JOIN (SELECT votable_id, SUM(value) AS score
-      FROM votes WHERE votable_type = \'Question\'
-      GROUP BY votable_id
-      ORDER BY score DESC) v ON questions.id = v.votable_id'
-      # Sub-join table with alias of v -- where v contains votable_id and sum of vote values (temp in memory) -- on questions.id and votable_id.
-    )
+    # @highest_voted = Question.joins(
+    #   # Question.joins = SELECT questions.* FROM questions
+    #   'JOIN (SELECT votable_id, SUM(value) AS score
+    #   FROM votes WHERE votable_type = \'Question\'
+    #   GROUP BY votable_id
+    #   ORDER BY score DESC) v ON questions.id = v.votable_id'
+    #   # Sub-join table with alias of v -- where v contains votable_id and sum of vote values (temp in memory) -- on questions.id and votable_id.
+    # )
   end
 
   def new
